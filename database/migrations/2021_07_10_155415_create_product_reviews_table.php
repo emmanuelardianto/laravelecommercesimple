c6964f27856e7a67eb5ca35b9184cdc864f96a53
Emmanuel Sitinjak <surabi.eman@gmail.com>
2021-07-11 00:58:07 +0900

    product review

---
+++ database/migrations/2021_07_10_155415_create_product_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('product_id');
+            $table->string('title', 250)->nullable();
+            $table->string('description', 1000)->nullable();
+            $table->tinyInteger('rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_reviews');
+    }
+}
