f770760c3a7befd98bfe13a99095f82912b2af3b
Emmanuel Sitinjak <surabi.eman@gmail.com>
2021-09-13 23:20:43 +0900

    product review table

--- database/migrations/2021_07_10_155415_create_product_reviews_table.php
+++ database/migrations/2021_07_10_155415_create_product_reviews_table.php
@@ -17,9 +17,19 @@ class CreateProductReviewsTable extends Migration
             $table->id();
             $table->integer('user_id');
             $table->integer('product_id');
+            $table->integer('product_variant_id');
+            $table->string('nick_name', 250)->nullable();
             $table->string('title', 250)->nullable();
             $table->string('description', 1000)->nullable();
+            $table->tinyInteger('comfort_rating');
             $table->tinyInteger('rating');
+            $table->string('gender')->nullable();
+            $table->string('size')->nullable();
+            $table->string('height_range')->nullable();
+            $table->string('weight_range')->nullable();
+            $table->string('shoe_size')->nullable();
+            $table->string('age_range')->nullable();
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
